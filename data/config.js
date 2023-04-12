5abdc8caf748fa8c56f3e0215c0bf7a9be2ea026
AhmedNurovic <ahmed.nurovic@hotmail.com>
2023-04-12 03:42:00 +0200

    Adjust projects

--- data/config.js
+++ data/config.js
@@ -10,19 +10,31 @@ export const hero = {
 
 export const projects = {
   title: 'My Projects',
-  desc: "Created and fixed components, worked with widgets and modules in integrated CMS and a lot of ther stuff",
+  desc: "I developed backend systems that enabled seamless communication through various channels, such as Telnyx for SMS chatting, WebSockets for real-time conversations, and SendGrid for email communication. Additionally, our platform offered integrations with YouTube's API for native video uploads and analytics, as well as LinkedIn's V2 API for authentication and data sharing between the two platforms. Our primary goal was to provide a unified communication experience, connecting users to their audience through multiple touchpoints and fostering meaningful interactions.To ensure the reliability and stability of our platform, I implemented Jest testing to carry out thorough unit and integration tests. I was also responsible for database management, including the creation of database migrations to handle schema changes and data manipulation effectively. This role required me to work closely with various components, widgets, and modules within an integrated CMS to build and refine the user experience.As part of a dynamic development team, I contributed to the creation and improvement of numerous components, collaborated on the design and implementation of custom widgets, and developed modules to extend the platform's functionality. My extensive work in these areas helped drive the continuous evolution of our platform, enabling us to adapt to user needs and deliver an engaging, high-quality communication experience.Write this text so that it fits the above param type",
   projects: [
+    {
+      title: 'Covideo',
+      description: 'I worked at Covideo, where I developed a backend system that enabled seamless communication through various channels, such as Telnyx for SMS chatting, WebSockets for real-time conversations, and SendGrid for email communication. Additionally, our platform offered integrations with YouTubes API for native video uploads and analytics, as well as LinkedIns V2 API for authentication and data sharing between the two platforms. Our primary goal was to provide a unified communication experience, connecting users to their audience through multiple touchpoints and fostering meaningful interactions.',
+      link: 'https://www.covideo.com/',
+      github: 'Live'
+    },
     {
       title: 'OAEW Verlag',
       description: 'Hotfixes, bugfixes, modules and components built',
       link: 'https://www.oeaw.ac.at/oesterreichische-akademie-der-wissenschaften',
-      github: 'Private Git'
+      github: 'Live'
     },
     {
       title: 'Handwerk und Technik',
-      description: 'Worked from the begging and implemented components and widgets from the ground up (still in development)',
+      description: 'Worked from the begging and implemented components and widgets from the ground up',
       link: 'https://www.handwerk-technik.de',
-      github: 'Under development'
+      github: 'Live'
+    },
+    {
+      title: 'Mein Kamishibai',
+      description: 'Worked from the begging and implemented components and widgets from the ground up',
+      link: 'https://www.mein-kamishibai.de/',
+      github: 'Live'
     }
   ]
 };
--- pages/_document.js
+++ pages/_document.js
@@ -7,19 +7,19 @@ export default class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicon/apple-touch-icon.png"
+            href="/static/favicon/favicon.ico"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicon/favicon-32x32.png"
+            href="/static/favicon/favicon.ico"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicon/favicon-16x16.png"
+            href="/static/favicon/favicon.ico"
           />
           <link rel="manifest" href="/static/favicon/site.webmanifest" />
         </Head>
